cf2dcc48889656781dd8b570733d1b2528bd54d0
Tristonvn <tristonvn@gmail.com>
2021-04-13 00:27:39 +0200

    Add project files.

---
+++ Rock_paper-scissors/Rock_paper-scissors.cpp
@@ -0,0 +1,11 @@
+/*  This projects aims to simulate a game of 
+    rock, paper, scissors, lizard, spock from 
+    the Big Bang Theory*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
