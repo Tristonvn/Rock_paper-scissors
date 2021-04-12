874a60201f2ae0ecd6da9131ebe879c38c7a3f89
Tristonvn <tristonvn@gmail.com>
2021-04-13 00:39:38 +0200

    complete project

--- Rock_paper-scissors/Rock_paper-scissors.cpp
+++ Rock_paper-scissors/Rock_paper-scissors.cpp
@@ -1,11 +1,60 @@
-/*  This projects aims to simulate a game of 
-    rock, paper, scissors, lizard, spock from 
-    the Big Bang Theory*/
+﻿/*  This projects aims to simulate a game of 
+    rock, paper, scissors*/
 
 #include <iostream>
+#include <stdlib.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(NULL));
+
+    int computer = rand() % 3 + 1;
+    
+    int user = 0;
+
+    std::cout << "====================\n";
+    std::cout << "rock paper scissors!\n";
+    std::cout << "====================\n";
+
+    std::cout << "1) ✊\n";
+    std::cout << "2) ✋\n";
+    std::cout << "3) ✌️\n";
+
+    std::cout << "shoot! \n";
+
+    std::cin >> user;
+    std::string outcome;
+
+    if (user == 1 && computer == 3) {
+
+        outcome = "You win!";
+        
+    }
+    else if (user == 2 && computer == 1) {
+        outcome = "You win!";
+        
+    }
+    else if (user == 3 && computer == 2) {
+        outcome = "You win!";
+       
+    } 
+    else if (user == 1 && computer == 2) {
+        outcome = "You lose!";
+        
+    }
+    else if (user == 2 && computer == 3) {
+        outcome = "You lose!";
+        
+    }
+    else if (user == 3 && computer == 1) {
+        outcome = "You lose!";
+        
+    } 
+    else {
+        outcome = "You tied!";
+    
+    }
+
+    std::cout << outcome;
 }
 
